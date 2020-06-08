6f8a1cd09dbaea534fb5b79e11429a4472fa35f0
18073833026 <xzp663@126.com>
2020-06-08 14:31:27 +0800

    完成了后台登录，管理的页面，功能还未实现

---
+++ 毕业项目源码和数据库/项目/ViewNews/ViewNews/Controllers/ViewNewsGuanLi/LoginVNGLController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewNews.Controllers.ViewNewsGuanLi
+{
+    public class LoginVNGLController : Controller
+    {
+        // GET: LoginVNGL
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
